364dc22d38a3c2d04325a9979af0d9ac9c976dee
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-04-04 14:38:40 -0400

    Frame for set-up and jwt.sign

---
+++ db.js
@@ -0,0 +1,96 @@
+const Sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
+const { STRING } = Sequelize;
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING) {
+  delete config.logging;
+}
+const conn = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  config
+);
+
+const User = conn.define('user', {
+  username: STRING,
+  password: STRING,
+});
+
+User.byToken = async (token) => {
+  try {
+    const user = await User.findByPk(token);
+    if (user) {
+      return user;
+    }
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw error;
+  } catch (ex) {
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw error;
+  }
+};
+
+User.authenticate = async ({ username, password }) => {
+  const user = await User.findOne({
+    where: {
+      username,
+      password,
+    },
+  });
+
+  console.log('🦐🦀🦞USER: ', user);
+  if (user) {
+    console.log(
+      '🦐🦀🦞 JWT SIGN: ',
+      jwt.sign({ userId: user.id }, process.env.JWT, { expiresIn: '1h' })
+    );
+    return jwt.sign({ userId: user.id }, process.env.JWT, { expiresIn: '1h' });
+  }
+  const error = Error('bad credentials');
+  error.status = 401;
+  throw error;
+};
+// User.authenticate = async ({ username, password }) => {
+//   const user = await User.findOne({
+//     where: {
+//       username,
+//       password,
+//     },
+//   });
+//   if (user) {
+//     return user.id;
+//   }
+//   const error = Error('bad credentials');
+//   error.status = 401;
+//   throw error;
+// };
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  const credentials = [
+    { username: 'lucy', password: 'lucy_pw' },
+    { username: 'moe', password: 'moe_pw' },
+    { username: 'larry', password: 'larry_pw' },
+  ];
+  const [lucy, moe, larry] = await Promise.all(
+    credentials.map((credential) => User.create(credential))
+  );
+  return {
+    users: {
+      lucy,
+      moe,
+      larry,
+    },
+  };
+};
+
+module.exports = {
+  syncAndSeed,
+  models: {
+    User,
+  },
+};
